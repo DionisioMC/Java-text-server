a13517adc05d3085ede421d8d244c9cf9087d544
Dionisio Coelho <dionisiomeireles@gmail.com>
2024-04-25 15:23:09 +0100

    Adding a banner to appear when you launch the server

--- src/io/codeforall/javatars/Server.java
+++ src/io/codeforall/javatars/Server.java
@@ -15,6 +15,32 @@ public class Server {
     public static void main(String[] args) throws IOException {
         int portNumber = 9000;
 
+        System.out.println("                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "        CCCCCCCCCCCCChhhhhhh                                       tttt                  SSSSSSSSSSSSSSS                                                                                                    \n" +
+                "     CCC::::::::::::Ch:::::h                                    ttt:::t                SS:::::::::::::::S                                                                                                   \n" +
+                "   CC:::::::::::::::Ch:::::h                                    t:::::t               S:::::SSSSSS::::::S                                                                                                   \n" +
+                "  C:::::CCCCCCCC::::Ch:::::h                                    t:::::t               S:::::S     SSSSSSS                                                                                                   \n" +
+                " C:::::C       CCCCCC h::::h hhhhh         aaaaaaaaaaaaa  ttttttt:::::ttttttt         S:::::S                eeeeeeeeeeee    rrrrr   rrrrrrrrrvvvvvvv           vvvvvvv eeeeeeeeeeee    rrrrr   rrrrrrrrr   \n" +
+                "C:::::C               h::::hh:::::hhh      a::::::::::::a t:::::::::::::::::t         S:::::S              ee::::::::::::ee  r::::rrr:::::::::rv:::::v         v:::::vee::::::::::::ee  r::::rrr:::::::::r  \n" +
+                "C:::::C               h::::::::::::::hh    aaaaaaaaa:::::at:::::::::::::::::t          S::::SSSS          e::::::eeeee:::::eer:::::::::::::::::rv:::::v       v:::::ve::::::eeeee:::::eer:::::::::::::::::r \n" +
+                "C:::::C               h:::::::hhh::::::h            a::::atttttt:::::::tttttt           SS::::::SSSSS    e::::::e     e:::::err::::::rrrrr::::::rv:::::v     v:::::ve::::::e     e:::::err::::::rrrrr::::::r\n" +
+                "C:::::C               h::::::h   h::::::h    aaaaaaa:::::a      t:::::t                   SSS::::::::SS  e:::::::eeeee::::::e r:::::r     r:::::r v:::::v   v:::::v e:::::::eeeee::::::e r:::::r     r:::::r\n" +
+                "C:::::C               h:::::h     h:::::h  aa::::::::::::a      t:::::t                      SSSSSS::::S e:::::::::::::::::e  r:::::r     rrrrrrr  v:::::v v:::::v  e:::::::::::::::::e  r:::::r     rrrrrrr\n" +
+                "C:::::C               h:::::h     h:::::h a::::aaaa::::::a      t:::::t                           S:::::Se::::::eeeeeeeeeee   r:::::r               v:::::v:::::v   e::::::eeeeeeeeeee   r:::::r            \n" +
+                " C:::::C       CCCCCC h:::::h     h:::::ha::::a    a:::::a      t:::::t    tttttt                 S:::::Se:::::::e            r:::::r                v:::::::::v    e:::::::e            r:::::r            \n" +
+                "  C:::::CCCCCCCC::::C h:::::h     h:::::ha::::a    a:::::a      t::::::tttt:::::t     SSSSSSS     S:::::Se::::::::e           r:::::r                 v:::::::v     e::::::::e           r:::::r            \n" +
+                "   CC:::::::::::::::C h:::::h     h:::::ha:::::aaaa::::::a      tt::::::::::::::t     S::::::SSSSSS:::::S e::::::::eeeeeeee   r:::::r                  v:::::v       e::::::::eeeeeeee   r:::::r            \n" +
+                "     CCC::::::::::::C h:::::h     h:::::h a::::::::::aa:::a       tt:::::::::::tt     S:::::::::::::::SS   ee:::::::::::::e   r:::::r                   v:::v         ee:::::::::::::e   r:::::r            \n" +
+                "        CCCCCCCCCCCCC hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa         ttttttttttt        SSSSSSSSSSSSSSS       eeeeeeeeeeeeee   rrrrrrr                    vvv            eeeeeeeeeeeeee   rrrrrrr            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "                                                                                                                                                                                                            \n" +
+                "\n");
         ServerSocket serverSocket = new ServerSocket(portNumber);
         Socket clientSocket;
 
